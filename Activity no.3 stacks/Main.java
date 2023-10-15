16acb93eb6702967f77d9c671254d45e4351fd73
kenfutamata <104425231+kenfutamata@users.noreply.github.com>
2023-10-15 10:50:33 +0800

    Add files via upload

--- Activity no.3 stacks/Main.java
+++ Activity no.3 stacks/Main.java
@@ -9,20 +9,9 @@ Scanner input = new Scanner(System.in);
 int size; 
 System.out.print("Enter Stack size: ");
 size = input.nextInt();
+cap j1 = new cap(size);
+j1.menu();
 
-while(true){
-System.out.println("--------Menu--------");
-System.out.println("[1] - isEmpty"); 
-System.out.println("[2] - Print"); 
-System.out.println("[3] - Push"); 
-System.out.println("[4] - Pop"); 
-System.out.println("[5] - Top"); 
-System.out.println("-------------------");
-System.out.println();
-System.out.print("Enter choice: "); 
-int choose = input.nextInt(); 
-
-}
 }
 
 
--- Activity no.3 stacks/cap.java
+++ Activity no.3 stacks/cap.java
@@ -1,33 +1,112 @@
+import java.util.*;
 public class cap{
-
+static Scanner input = new Scanner(System.in);
 int size;
-int arr[] = new int[size]; 
+int arr[];
 int choose; 
 int top =-1; 
-public cap(int size, int choose){
-this.size = size; 
-this.choose = choose; 
+public cap(int size){
+arr = new int[size];
+
+}
+
+public void menu() {
+    while (true) {
+        System.out.println("--------Menu--------");
+        System.out.println("[1] - isEmpty");
+        System.out.println("[2] - Print");
+        System.out.println("[3] - Push");
+        System.out.println("[4] - Pop");
+        System.out.println("[5] - Top");
+        System.out.println("-------------------");
+        System.out.println();
+        System.out.print("Enter choice: ");
+        int choose = input.nextInt();
+        choose(choose);
+    }
+
+}
+public void choose(int choose){
+    Scanner input = new Scanner(System.in);
+if(choose ==1){
+    isEmpty();
+}else if(choose == 2){
+    print();
+}else if(choose == 3) {
+elementpush();
+}
+
+else if(choose ==4){
+    pop();
+}
+else if(choose == 5){
+    top();
+}
 }
 
 // 1. isEmpty()
-public void isempty(int arr[]){
+public boolean isEmpty() {
 
-if(arr == null){
-System.out.print("The array is empty and null"); 
-}
+    return (top == -1);
 }
 
 // 2. Print()
 
 public void print(){
-if(top== -1){
-System.out.print("Stack is empty"); 
-}else{
-System.out.println("Array elements");
-for(int i = 0; i < this.size; i++){
-System.out.println("");
-}
-}
+   //arr = new int[size];
+    System.out.println("Stack elements from top to bottom: ");
+    for (int i = top; i >= 0; i--) {
+        System.out.println(arr[i]);
+    }
 
 }
+
+
+
+
+// 3. Push()
+
+public void push(int num){
+    top++;
+    arr[top] = num;
+    System.out.println("Element contents: "+num);
+}
+
+public void elementpush(){
+
+    if(top == arr.length-1){
+        System.out.println("Stack is full!");
+    }else {
+        try{
+            System.out.println("Enter an element: ");
+            int element  = input.nextInt();
+            push(element);
+        }catch (InputMismatchException e){
+            System.out.println("Invalid input!");
+            input.nextInt();
+        }
+    }
+}
+
+// 4. Pop()
+public void pop(){
+    if(top == -1){
+        System.out.println("Stack is empty!");
+    }else{
+        arr[top--] = 0;
+    }
+}
+
+//5. Top()
+
+    public int top(){
+    if(top == -1){
+        System.out.println("Stack is empty");
+        return -1;
+    }else{
+        System.out.println("Topped element: "+arr[top]);
+        return arr[top];
+
+    }
+    }
 }
